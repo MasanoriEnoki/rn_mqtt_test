4e31def587ad5b626deb91d9072305d4e68026b4
Masanori Enoki <enoki@adawarp.com>
2020-08-03 19:31:15 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,175 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  // AsyncStorage,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+// import init from 'react_native_mqtt';
+// import AsyncStorage from '@react-native-community/async-storage';
+
+// init({
+//   size: 10000,
+//   storageBackend: AsyncStorage,
+//   defaultExpires: 1000 * 3600 * 24,
+//   enableCache: true,
+//   reconnect: true,
+//   sync : {
+//   }
+// });
+
+// function onConnect() {
+//   console.log("onConnect");
+// }
+
+// function onConnectionLost(responseObject) {
+//   if (responseObject.errorCode !== 0) {
+//     console.log("onConnectionLost:"+responseObject.errorMessage);
+//   }
+// }
+
+// function onMessageArrived(message) {
+//   console.log("onMessageArrived:"+message.payloadString);
+// }
+
+// const client = new Paho.MQTT.Client('iot.eclipse.org', 443, 'uname');
+// client.onConnectionLost = onConnectionLost;
+// client.onMessageArrived = onMessageArrived;
+// client.connect({ onSuccess:onConnect, useSSL: true });
+// size: 10000,
+// storageBackend: AsyncStorage,
+// defaultExpires: 1000 * 3600 * 24,
+// enableCache: true,
+// reconnect: true,
+// sync : {
+// }
+// });
+
+// function onConnect() {
+// console.log("onConnect");
+// }
+
+// function onConnectionLost(responseObject) {
+// if (responseObject.errorCode !== 0) {
+//   console.log("onConnectionLost:"+responseObject.errorMessage);
+// }
+// }
+
+// function onMessageArrived(message) {
+// console.log("onMessageArrived:"+message.payloadString);
+// }
+
+// const client = new Paho.MQTT.Client('iot.eclipse.org', 443, 'uname');
+// client.onConnectionLost = onConnectionLost;
+// client.onMessageArrived = onMessageArrived;
+// client.connect({ onSuccess:onConnect, useSSL: true });
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
